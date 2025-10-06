e31f23e6cca9334e70db11d70085fdc7dfdae137
jude <lysaghtjude@gmail.com>
2025-10-06 10:39:13 +0100

    sparks

--- gun.gd
+++ gun.gd
@@ -1,12 +1,14 @@
 extends MeshInstance3D
 
-@onready var shoot:GPUParticles3D = $shoot/GPUParticles3D
+@onready var shoot:GPUParticles3D = $shoot/Tracers
+@export var rate:int = 8
 
 func _ready():
-	print("ready")
+	shoot.amount = rate
 
 func _physics_process(_delta):
 	
 	if Input.is_action_pressed("shoot"):
-		print("w")
-		self.shoot.restart()
+		shoot.emitting = true
+	else:
+		shoot.emitting = false
